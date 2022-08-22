4638363e0f5a884848f00ba2a93c3a4277daae16
07arif <ai.arif0711@gmail.com>
2022-08-22 18:28:28 +0600

    js chalange added

---
+++ practice.js
@@ -0,0 +1,4 @@
+document.getElementById('btn-buyNow').addEventListener('click', function () {
+    const headingTitle = document.getElementById('heding-title');
+    headingTitle.innerText = 'Im the penguin';
+})
